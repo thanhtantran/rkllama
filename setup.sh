d792df84471c7a1b42a6275d8975a105fe01eba4
notpunchnox <punchnoxpro@gmail.com>
2024-12-31 18:23:08 +0100

    Fix cp . -rf ~/RKLLAMA

--- setup.sh
+++ setup.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Vérification de mise à jours
-echo "${CYAN}Vérification de mise à jours...${RESET}"
-git pull
-echo "${GREEN}Vérification des mises à jours terminé avec succès!${RESET}"
-
 # Définition des couleurs
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -12,6 +7,11 @@ YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 CYAN='\033[0;36m'
 RESET='\033[0m'
+
+# Vérification de mise à jour du dépôt Git
+echo -e "${CYAN}Vérification de mise à jour...${RESET}"
+git pull
+echo -e "${GREEN}Vérification des mises à jour terminée avec succès!${RESET}"
 
 # Chemin d'installation de Miniconda
 MINICONDA_DIR=~/miniconda3
@@ -29,34 +29,34 @@ else
     echo -e "${GREEN}Miniconda a été installé avec succès.${RESET}"
 fi
 
-echo "${CYAN}Création du répertoire ~/RKLLAMA...${RESET}"
-mkdir ~/RKLLAMA/
-echo "${CYAN}Installation de répertoire dans le dossier ~/RKLLAMA...${RESET}"
-cp . ~/RKLLAMA/
+# Création du répertoire RKLLAMA
+echo -e "${CYAN}Création du répertoire ~/RKLLAMA...${RESET}"
+mkdir -p ~/RKLLAMA/
+echo -e "${CYAN}Installation des ressources dans ~/RKLLAMA...${RESET}"
+cp -rf . ~/RKLLAMA/
 
 # Activation de Miniconda
 source "$MINICONDA_DIR/bin/activate"
 
 # Installation des dépendances
 echo -e "${CYAN}Installation des dépendances depuis requirements.txt...${RESET}"
-pip3 install -r requirements.txt
+pip3 install -r ~/RKLLAMA/requirements.txt
 
 # Rendre client.sh et server.sh exécutable
 echo -e "${CYAN}Rendre ./client.sh et ./server.sh exécutable${RESET}"
-chmod +x ./server.sh
-chmod +x ./client.sh
+chmod +x ~/RKLLAMA/client.sh
+chmod +x ~/RKLLAMA/server.sh
 
-# Exporter client
-echo -e "${CYAN}Création d'un exécutable global pour rkllama..${RESET}"
-sudo cp ./client.sh /usr/local/bin/rkllama
+# Exporter client.sh en tant que commande globale
+echo -e "${CYAN}Création d'un exécutable global pour rkllama...${RESET}"
+sudo cp ~/RKLLAMA/client.sh /usr/local/bin/rkllama
 sudo chmod +x /usr/local/bin/rkllama
-echo -e "${CYAN}Exécutable créé avec succès: /usr/local/bin/client${RESET}"
+echo -e "${CYAN}Exécutable créé avec succès: /usr/local/bin/rkllama${RESET}"
 
-#Afficher les status et commandes désormais disponibles
+# Affichage des statuts et des commandes désormais disponibles
 echo -e "${GREEN}+ Configuration : OK.${RESET}"
 echo -e "${GREEN}+ Installation  : OK.${RESET}"
 
 echo -e "${BLUE}Serveur${GREEN}  : ./server.sh${RESET}"
 echo -e "${BLUE}Client${GREEN}   : ./client.sh${RESET}\n"
-
-echo -e "${BLUE}Commande global  : ${RESET}rkllama"
+echo -e "${BLUE}Commande globale  : ${RESET}rkllama"
