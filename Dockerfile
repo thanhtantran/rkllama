6ff7cb6d9d846134ada06b0beb5c1ea1cc011613
Ichlasul Affan <ichlaffterlalu@gmail.com>
2025-02-15 09:13:19 +0700

    Updated requirements, set port to 8080, and add CPU_MODEL env.

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,16 @@
-FROM --platform=linux/arm64 python:slim as build
+FROM python:slim
 
 RUN apt-get update \
-    && apt-get install -y --no-install-recommends wget curl sudo \
+    && apt-get install -y --no-install-recommends libgomp1 wget curl sudo \
     && rm -rf /var/cache/apt/archives /var/lib/apt/lists/*
 
 WORKDIR /opt/rkllama
 
-COPY . /opt/rkllama/
+COPY ./lib /opt/rkllama/lib
+COPY ./src /opt/rkllama/src
+COPY requirements.txt README.md LICENSE *.sh *.py /opt/rkllama/
 RUN chmod +x setup.sh && ./setup.sh
 
-EXPOSE 5000
+EXPOSE 8080
 
-CMD ["rkllama", "serve"]
+CMD ["/usr/local/bin/rkllama", "serve"]
--- server.sh
+++ server.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Extracting the CPU model
-cpu_model=$(cat /proc/cpuinfo | grep "cpu model" | head -n 1 | sed 's/.*Rockchip \(RK[0-9]*\).*/\1/' | tr '[:upper:]' '[:lower:]')
+# Extracting the CPU model if specific CPU model is not defined in env
+if [ -z "$CPU_MODEL" ] && [ "${CPU_MODEL+xxx}" = "xxx" ]; then
+  CPU_MODEL=$(cat /proc/cpuinfo | grep "cpu model" | head -n 1 | sed 's/.*Rockchip \(RK[0-9]*\).*/\1/' | tr '[:upper:]' '[:lower:]')
+fi
 
 # Starting the server with the CPU model as a parameter
-python3 ~/RKLLAMA/server.py --target_platform "$cpu_model"
+python3 ~/RKLLAMA/server.py --target_platform "$CPU_MODEL"
